835e31a8347fce1ff6b2cf9754f2089de19b4a9d
Zhen Liu <novarholland@gmail.com>
2024-09-23 11:23:16 -0500

    Initialization

---
+++ Codes/ModelFile/ALP_linear_UFO_SK/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.0.0 for Mac OS X x86 (64-bit) (April 7, 2019)
+# Date: Sun 15 Nov 2020 07:01:42
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 2)
+
